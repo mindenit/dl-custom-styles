1648d4b168b8a6345170c45bcd3bbce6f05c8d14
perkinson1251 <tttrashutin@gmail.com>
2024-11-28 12:24:28 +0200

    feat: new fixes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const sectionToRemove = document.querySelector('#section-1')?.remove();
 const siteNewsForum = document.querySelector('#site-news-forum')?.remove();
 const supportemail = document.querySelector('.supportemail')?.remove();
 const footerButton = document.querySelector('.btn-footer-popover')?.remove();
